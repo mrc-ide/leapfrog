490abb3c5bb919b9c4f4730073f26f44aca808b8
Robert Ashton <r.ashton@imperial.ac.uk>
2022-06-08 14:03:17 +0100

    Remove unnecessary files and tidy code after review

--- inst/include/code.h
+++
@@ -1,8 +0,0 @@
-#ifndef FROGGER_H
-#define FROGGER_H
-
-inline int add(int a, int b) {
-  return a + b;
-}
-
-#endif //FROGGER_H
---
+++ inst/include/code.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+inline int add(int a, int b) {
+  return a + b;
+}
