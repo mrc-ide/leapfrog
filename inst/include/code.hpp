8a19ed53603ca097e5a56c6948f65125e34bcd44
Robert Ashton <r.ashton@imperial.ac.uk>
2022-06-13 15:27:39 +0100

    Add Rcpp wrapper

--- inst/include/code.hpp
+++ inst/include/code.hpp
@@ -1,5 +1,36 @@
 #pragma once
 
-inline int add(int a, int b) {
-  return a + b;
-}
+#include <unsupported/Eigen/CXX11/Tensor>
+
+template <typename real_type>
+using TensorMapX1cT = Eigen::TensorMap<Eigen::Tensor<const real_type, 1>>;
+
+template <typename real_type>
+using TensorMapX2cT = Eigen::TensorMap<Eigen::Tensor<const real_type, 2>>;
+
+template <typename real_type>
+using TensorMapX1T = Eigen::TensorMap<Eigen::Tensor<real_type, 1>>;
+
+template <typename real_type>
+using TensorMapX2T = Eigen::TensorMap<Eigen::Tensor<real_type, 2>>;
+
+template <typename real_type>
+struct Parameters {
+  int num_genders;
+  int age_groups_pop;             // Default 81 for ages 0 to 80+
+  int fertility_first_age_group;  // First index eligible for fertility
+  int age_groups_fert;            // Number of ages eligible for fertility
+
+  TensorMapX2T<real_type> base_pop;
+  TensorMapX2T<real_type> survival;
+  TensorMapX2T<real_type> net_migration;
+  TensorMapX1T<real_type> asfr;
+  TensorMapX1T<real_type> births_sex_prop;
+};
+
+template <typename real_type>
+struct State {
+  TensorMapX2T<real_type> total_population;
+  real_type births;
+  TensorMapX2T<real_type> natural_deaths;
+};
--- src/code.cpp
+++ src/code.cpp
@@ -2,13 +2,63 @@
 
 #include "code.hpp"
 
-//' Add two numbers.
-//'
-//' @param a First number.
-//' @param b Second number.
-//' @return The sum of two values.
-//' @export
 // [[Rcpp::export]]
-int adder(int a, int b) {
-  return add(a, b);
+Rcpp::List run_base_model(const Rcpp::List demp) {
+  const size_t proj_years =
+      2;  // TODO: Fix temporarily for testing should be d[2]
+  const int num_genders = 2;
+  const int age_groups_pop = 81;
+  const int fertility_first_age_group = 15;
+  const int age_groups_fert = 35;
+
+  TensorMapX2T<double> base_pop(REAL(demp["basepop"]), age_groups_pop,
+                                num_genders);
+  TensorMapX2T<double> survival(REAL(demp["Sx"]), age_groups_pop + 1,
+                                num_genders);
+  TensorMapX2T<double> net_migration(REAL(demp["netmigr_adj"]), age_groups_pop,
+                                     num_genders);
+  TensorMapX1T<double> age_sex_fertility_ratio(REAL(demp["asfr"]),
+                                               age_groups_fert);
+  TensorMapX1T<double> births_sex_prop(REAL(demp["births_sex_prop"]),
+                                       num_genders);
+
+  Parameters<double> params = {num_genders,
+                               age_groups_pop,
+                               fertility_first_age_group,
+                               age_groups_fert,
+                               base_pop,
+                               survival,
+                               net_migration,
+                               age_sex_fertility_ratio,
+                               births_sex_prop};
+
+  // allocate memory for return object
+  Rcpp::NumericVector total_population(age_groups_pop * num_genders);
+  total_population.attr("dim") =
+      Rcpp::NumericVector::create(age_groups_pop, num_genders);
+  double births;
+
+  Rcpp::NumericVector natural_deaths(age_groups_pop * num_genders);
+  natural_deaths.attr("dim") =
+      Rcpp::NumericVector::create(age_groups_pop, num_genders);
+
+  TensorMapX2T<double> total_population_tensor(REAL(total_population),
+                                               age_groups_pop, num_genders);
+  TensorMapX2T<double> natural_deaths_tensor(REAL(natural_deaths),
+                                             age_groups_pop, num_genders);
+
+  State<double> initial_state = {total_population_tensor, births,
+                                 natural_deaths_tensor};
+
+  // leapfrog_sim<double>(
+  //     REAL(demp["basepop"]), REAL(demp["Sx"]), REAL(demp["netmigr_adj"]),
+  //     REAL(demp["asfr"]), REAL(demp["births_sex_prop"]), num_genders,
+  //     age_groups_pop, fertility_first_age_group, age_groups_fert, proj_years,
+  //     REAL(total_population), REAL(births), REAL(natural_deaths));
+
+  Rcpp::List ret = Rcpp::List::create(
+      Rcpp::_["total_population"] = total_population,
+      Rcpp::_["births"] = births, Rcpp::_["natural_deaths"] = natural_deaths);
+
+  return ret;
 }
