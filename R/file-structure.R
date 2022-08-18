273b9b3804d32254c140716472ecb4120060a841
maggie <mwalters@imperial.ac.uk>
2022-08-18 10:10:39 +0100

    added package specifications

--- R/file-structure.R
+++ R/file-structure.R
@@ -1,5 +1,5 @@
 get_coarse_ages_lfrog <- function(input){
-  sample_fp <- eppasm::prepare_directincid(test_path("../testdata/spectrum/v6.13/bwa_demproj-only_spectrum-v6.13_2022-02-12.PJNZ"))
+  sample_fp <- eppasm::prepare_directincid(testthat::test_path("../testdata/spectrum/v6.13/bwa_demproj-only_spectrum-v6.13_2022-02-12.PJNZ"))
   age_mapping <- data.frame(
     eppasm_age = sample_fp$ss$ag.idx,
     leapfrog_age = 1:66
