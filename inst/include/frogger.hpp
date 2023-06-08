249a71d99ed6baa407e964c7462f78135322f873
Robert Ashton <r.ashton@imperial.ac.uk>
2023-06-08 17:17:57 +0100

    Reset more indicators

--- inst/include/frogger.hpp
+++ inst/include/frogger.hpp
@@ -49,6 +49,8 @@ State<real_type> run_model(int time_steps, const Parameters<real_type> &pars) {
     state_next.aids_deaths_no_art.setZero();
     state_next.infections.setZero();
     state_next.hiv_deaths.setZero();
+    state_next.aids_deaths_art.setZero();
+    state_next.art_initiation.setZero();
     internal::run_general_pop_demographic_projection(step, pars, state, state_next, intermediate);
     internal::run_hiv_pop_demographic_projection(step, pars, state, state_next, intermediate);
     internal::run_hiv_model_simulation(step, pars, state, state_next, intermediate);
