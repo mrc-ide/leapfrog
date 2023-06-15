e248c4d32a9311ddd924b7d5e224a16fed41e016
Rob Ashton <r.ashton@imperial.ac.uk>
2023-06-15 17:13:54 +0100

    Document serialization utils properly

--- R/util.R
+++ R/util.R
@@ -2,8 +2,15 @@
   if (is.null(x)) y else x
 }
 
-## Serialize an R array into a format that can be read as an Eigen::Tensor
-## by deserialize_tensor
+#' Serialize an R array into a format that can be read as an Eigen::Tensor
+#' by deserialize_tensor
+#'
+#' @param data The data to serialize, should be an array or vector
+#' @param path File path to serialize data to
+#'
+#' @return Path to file
+#'
+#' @keywords internal
 serialize_r_to_tensor <- function(data, path) {
   type <- "double"
   if (is.integer(data)) {
@@ -25,6 +32,13 @@ serialize_r_to_tensor <- function(data, path) {
   path
 }
 
+#' Deseralize an Eigen::Tensor into an R array
+#'
+#' @param path Path to serialized data
+#'
+#' @return An R array containing the deserialized data
+#'
+#' @keywords internal
 deserialize_tensor_to_r <- function(path) {
   content <- readLines(path)
   array(unlist(read.csv(text = content[3], header = FALSE), use.names = FALSE),
