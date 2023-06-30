1fc27925a874c158b671640d5a36a4e5faad560e
Robert Ashton <r.ashton@imperial.ac.uk>
2023-06-30 14:21:11 +0100

    Move fine/coarse input data switching into R wrapper

---
+++ R/run_model.R
@@ -0,0 +1,20 @@
+run_model <- function(data, parameters, sim_years,
+                      hiv_steps_per_year, output_steps,
+                      hiv_age_stratification = "full") {
+  assert_enum(hiv_age_stratification, c("full", "coarse"))
+  if (hiv_age_stratification == "full") {
+    parameters$hAG_SPAN <- parameters[["hAG_SPAN_full"]]
+    parameters$cd4_initdist <- parameters[["cd4_initdist_full"]]
+    parameters$cd4_prog <- parameters[["cd4_prog_full"]]
+    parameters$cd4_mort <- parameters[["cd4_mort_full"]]
+    parameters$art_mort <- parameters[["art_mort_full"]]
+  } else {
+    parameters$hAG_SPAN <- parameters[["hAG_SPAN_coarse"]]
+    parameters$cd4_initdist <- parameters[["cd4_initdist_coarse"]]
+    parameters$cd4_prog <- parameters[["cd4_prog_coarse"]]
+    parameters$cd4_mort <- parameters[["cd4_mort_coarse"]]
+    parameters$art_mort <- parameters[["art_mort_coarse"]]
+  }
+  run_base_model(data, parameters, sim_years, hiv_steps_per_year, output_steps,
+                 hiv_age_stratification)
+}
