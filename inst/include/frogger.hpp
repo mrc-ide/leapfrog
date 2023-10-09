2726c0ca53e2476b72704fe012bf8f484a1b4d7e
maggie <mwalters@imperial.ac.uk>
2023-10-09 15:39:35 +0100

    having issues, pushing to work on tomorrow during leapfrog check in

--- inst/include/frogger.hpp
+++ inst/include/frogger.hpp
@@ -6,6 +6,8 @@
 #include "child_model_simulation.hpp"
 #include "state_saver.hpp"
 #include "state_space.hpp"
+#include "population_adjustment.hpp"
+
 
 namespace leapfrog {
 
@@ -37,7 +39,7 @@ OutputState<ModelVariant, real_type> run_model(int time_steps,
                                                      intermediate);
     run_hiv_model_simulation<ModelVariant>(step, pars, state, state_next, intermediate);
  //   if constexpr (demog.pop_adjust) {
-      run_base_population_adjustment<ModelVariant>(step, pars, state, state_next, intermediate);
+       run_base_population_adjustment<ModelVariant>(step, pars, state, state_next, intermediate);
   //  }
     if constexpr (ModelVariant::run_child_model) {
       run_child_model_simulation<ModelVariant>(step, pars, state, state_next, intermediate);
