29879512fa1de3add31bf97737a6e5f0f7097013
maggie <mwalters@imperial.ac.uk>
2023-10-30 13:28:09 +0000

    updating parameters

--- scripts/create_test_data.R
+++ scripts/create_test_data.R
@@ -3,6 +3,8 @@ library(leapfrog)
 library(data.table)
 library(dplyr)
 
+setwd('C:/Users/mwalters/frogger/')
+
 ## Create demographic and projection parameters for adults
 pjnz1 <- testthat::test_path("testdata/bwa_aim-no-special-elig.PJNZ")
 
@@ -11,7 +13,7 @@ saveRDS(demp, testthat::test_path("testdata/demographic_projection_object_adult.
 proj <- prepare_leapfrog_projp(pjnz1)
 saveRDS(proj, testthat::test_path("testdata/projection_parameters_adult.rds"))
 
-## Used as reference data
+## Used as reference data (Run from leapfrog/master)
 lmod <- leapfrogR(demp, proj)
 saveRDS(lmod, testthat::test_path("testdata/leapfrog_fit.rds"))
 
@@ -22,45 +24,41 @@ mod <- leapfrogR(demp, proj, hiv_steps_per_year = 0L)
 saveRDS(lmod, testthat::test_path("testdata/fit_demography.rds"))
 
 
-# ## TODO: Add details about child model input data
-#
-# Test data coming from the same file but on the 'clean' branch of leapfrog.
-#
-# ```R
-# pjnz1 <- test_path("../testdata/spectrum/v6.13/bwa_aim-adult-art-no-special-elig_v6.13_2022-04-18.PJNZ")
-#
-# demp <- prepare_leapfrog_demp(pjnz1)
-# hivp <- prepare_leapfrog_projp(pjnz1)
-# hivp = prepare_hc_leapfrog_projp(pjnz1, hivp)
+#Create paeds parameters (Run from leapfrog/clean)
+demp <- prepare_leapfrog_demp(pjnz1)
+proj <- prepare_leapfrog_projp(pjnz1)
+proj <- leapfrog:::prepare_hc_leapfrog_projp(pjnz1, proj)
+lmod <- leapfrogR(demp, proj)
+
 #
 # demp$netmigr <- read_netmigr(pjnz1, adjust_u5mig = FALSE)
 # demp$netmigr_adj <- adjust_spectrum_netmigr(demp$netmigr)
-# hivp$incidinput[which(1970:2030 == 2000):length(parameters$incidinput)] <- 0
+# proj$incidinput[which(1970:2030 == 2000):length(parameters$incidinput)] <- 0
 #
-# hivp$paed_cd4_dist <- c(1,rep(0,6))
+# proj$paed_cd4_dist <- c(1,rep(0,6))
 #
 # ##HIV starts in 2000
-# hivp$paed_incid_input[] <- 0
-# hivp$paed_incid_input[which(1970:2030 ==2000)] <- 100
+# proj$paed_incid_input[] <- 0
+# proj$paed_incid_input[which(1970:2030 ==2000)] <- 100
 #
 # ##Only transmission is coming from nosocomial infections
-# hivp$pmtct_mtct[] <- 0
-# hivp$art_mtct[] <- 0
+# proj$pmtct_mtct[] <- 0
+# proj$art_mtct[] <- 0
 #
 # ##Add in treatment
-# hivp$paed_art_val[which(1970:2030 == 2002)] <- 50
-# hivp$artpaeds_isperc[] <- FALSE
+# proj$paed_art_val[which(1970:2030 == 2002)] <- 50
+# proj$artpaeds_isperc[] <- FALSE
 #
 # ##Change things to length 61
-# hivp$artpaeds_isperc <- c(hivp$artpaeds_isperc, FALSE)
-# hivp$paed_art_elig_age <- c(hivp$paed_art_elig_age, 2)
-# hivp$paed_art_elig_cd4 <- cbind(hivp$paed_art_elig_cd4, hivp$paed_art_elig_cd4[,ncol(hivp$paed_art_elig_cd4)])
+# proj$artpaeds_isperc <- c(proj$artpaeds_isperc, FALSE)
+# proj$paed_art_elig_age <- c(proj$paed_art_elig_age, 2)
+# proj$paed_art_elig_cd4 <- cbind(proj$paed_art_elig_cd4, proj$paed_art_elig_cd4[,ncol(proj$paed_art_elig_cd4)])
 #
-# hivp$laf = 1
+# proj$laf = 1
 #
 #
 # setwd('C:/Users/mwalters/frogger/tests/testthat/testdata/')
-# saveRDS(hivp, "projection_parameters_child.rds")
+# saveRDS(proj, "projection_parameters_child.rds")
 # saveRDS(demp, "demographic_projection_object_child.rds")
 #
 # ```
