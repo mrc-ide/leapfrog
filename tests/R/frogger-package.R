2c435b3394e82331e580010eaa942db007e2b918
maggie <mwalters@imperial.ac.uk>
2024-05-09 10:19:24 +0100

    removed extra copy of the package

--- tests/R/frogger-package.R
+++
@@ -1,5 +0,0 @@
-## usethis namespace: start
-#' @useDynLib frogger, .registration = TRUE
-#' @importFrom Rcpp sourceCpp
-## usethis namespace: end
-NULL
--- tests/R/zzz.R
+++
@@ -1,3 +0,0 @@
-.onUnload <- function(libpath) { # nolint # nocov start
-  library.dynam.unload("frogger", libpath)
-} # nocov end
