3bc93ee823c14287c230985e9a908015b411b3ff
Rob Ashton <robertashton94@gmail.com>
2024-06-05 14:39:00 +0100

    Add configure script to automatically update interface on package install or build

---
+++ inst/cpp_generation/generate.R
@@ -0,0 +1,24 @@
+#!/usr/bin/env Rscript
+
+message("* Generating R interfaces")
+## Source files manually so that if we break our generated code
+## we can still regenerate the interfaces without compiling
+root <- here::here()
+source(file.path(root, "R/util.R"))
+source(file.path(root, "R/util_assert.R"))
+source(file.path(root, "R/generate_cpp_validation.R"))
+source(file.path(root, "R/generate_cpp.R"))
+
+frogger_file <- function(..., mustWork = TRUE) {
+  path <- file.path(root, "inst", ...)
+  if (mustWork && !file.exists(path)) {
+    stop(sprintf("File at path %s does not exist", path))
+  }
+  path
+}
+
+generate_input_interface(dest = frogger_file("include/model_input.hpp"))
+generate_output_interface(dest = frogger_file("include/model_output.hpp"))
+generate_parameter_types(dest = frogger_file("include/parameter_types.hpp"))
+generate_state_types(dest = frogger_file("include/state_types.hpp"))
+generate_state_saver_types(dest = frogger_file("/include/generated/state_saver_types.hpp"))
