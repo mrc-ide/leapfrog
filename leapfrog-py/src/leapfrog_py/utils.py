8d59cd59bab55935b872543a69a1131209be35cc
Rob Ashton <robertashton94@gmail.com>
2024-06-13 15:54:30 +0100

    Add hatch lint scripts, and coverage, add to README

--- leapfrog-py/src/leapfrog_py/utils.py
+++ leapfrog-py/src/leapfrog_py/utils.py
@@ -1,13 +1,17 @@
+from typing import Any
+
 import numpy as np
 
 
-def assert_keys(data: dict[str, any], keys: list[str]):
+def assert_keys(data: dict[str, Any], keys: list[str]):
     missing_keys = [key for key in keys if key not in data.keys()]
     if missing_keys:
-        raise ValueError(f"Missing required keys: {', '.join(missing_keys)}")
+        msg = f"Missing required keys: {', '.join(missing_keys)}"
+        raise ValueError(msg)
 
 
 def assert_fortran_order(data: dict[str, np.ndarray]):
     for key, value in data.items():
-        if value.flags['F_CONTIGUOUS'] is False:
-            raise ValueError(f"Array '{key}' is not in Fortran order")
+        if value.flags["F_CONTIGUOUS"] is False:
+            msg = f"Array '{key}' is not in Fortran order"
+            raise ValueError(msg)
