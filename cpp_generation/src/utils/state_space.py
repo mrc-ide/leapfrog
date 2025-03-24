cf7bda88a473aacd13fd04363eb7d42cc60811aa
Mantra <mantrakusumgar@outlook.com>
2025-03-24 14:17:02 +0000

    remove unnecessary function from state_space utils

--- cpp_generation/src/utils/state_space.py
+++ cpp_generation/src/utils/state_space.py
@@ -1,14 +1,3 @@
-def get_template_specialisation(name, ss):
-  if ss.get("requires"):
-    collapsed_requires = " && ".join([f'ModelVariant::{req}' for req in ss["requires"]])
-    return (
-      f'requires({collapsed_requires})\n'
-      f'struct {name}SS<ModelVariant>'
-    )
-  else:
-    return f'struct {name}SS'
-
-
 def get_member_type(val):
   if isinstance(val, int):
     return "int"
