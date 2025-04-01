ba4519a0801287d07d6af897508befd1b14af39a
Rob Ashton <robertashton94@gmail.com>
2025-04-01 14:26:24 +0100

    Get C++ model running without R headers

--- inst/include/utils/input_utils.hpp
+++ inst/include/utils/input_utils.hpp
@@ -5,6 +5,7 @@
 
 #ifdef CALLER_R
 #include <Rcpp.h>
+#include "../generated/name_maps.hpp"
 
 template <typename T>
 T* r_data(SEXP x) {
@@ -22,22 +23,24 @@ int* r_data(SEXP x) {
 }
 
 template<typename T, typename... Args>
-auto parse_data(const InputData &data, const std::string& key, Args... dims) {
+auto parse_data(const InputData &data, const std::string& cpp_name, Args... dims) {
   constexpr std::size_t rank = sizeof...(dims);
+  // TODO: assert this name is valid and return good error if it is not
+  const std::string& r_name = leapfrog::CPP_TO_R_NAME.at(cpp_name);
   if constexpr (rank == 0) {
-      return Rcpp::as<T>(data[key]);
+      return Rcpp::as<T>(data[r_name]);
   } else {
 
     Eigen::array<int, rank> dimensions{ static_cast<int>(dims)... };
 
     int length = std::accumulate(dimensions.begin(), dimensions.end(), 1, std::multiplies<int>());
-    SEXP array_data = data[key];
+    SEXP array_data = data[r_name];
     // In cases where the input data has project years we might not use all of it model fit
     // So we can take create a Map over a smaller slice of the data
     // As long as this is true we can be confident we're not referencing invalid memory
     if (LENGTH(array_data) < length) {
         Rcpp::stop("Invalid size of data for '%s', expected %d got %d",
-                key,
+                r_name,
                 length,
                 LENGTH(array_data));
     }
