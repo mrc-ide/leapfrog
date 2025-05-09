c90fe0858314cdfb4b45361f08709d569f62a8da
Rob <39248272+r-ash@users.noreply.github.com>
2025-05-09 17:19:16 +0100

    Tidy up the build script, flesh out README

--- c_interface/README.md
+++ c_interface/README.md
@@ -1,5 +1,13 @@
 # leapfrog c
 
+This dir contains a interface to compile a C-compatible DLL. We use it to call the model from Delphi. You can build the DLL using the helper script
+
+```
+./build_dll.sh
+```
+
+Or manually
+
 ```
 cmake -A Win32 -B build
 cmake --build build
--- c_interface/build_dll.sh
+++ c_interface/build_dll.sh
@@ -7,11 +7,5 @@ echo $current
 trap "cd $current" EXIT
 
 here="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd $here
 
-cmake --build build
-
-# TODO: Copying into Rob's specific directory structure and won't work for everyone
-# let's think about how to manage this more generally
-echo "Copying DLL into place"
-cp build/Debug/leapfrog.dll $here/../../Spec5/
+cd $here && cmake --build build
