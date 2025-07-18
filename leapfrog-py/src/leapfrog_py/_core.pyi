a2bf65565799883ae706bb4a69ec9fcfc44f8805
Mantra <mantrakusumgar@outlook.com>
2025-07-18 20:54:41 +0100

    add python interface

--- leapfrog-py/src/leapfrog_py/_core.pyi
+++ leapfrog-py/src/leapfrog_py/_core.pyi
@@ -1,1 +1,20 @@
-def return_20() -> int: ...
+def run_base_model(
+    parameters: dict,
+    configuration: str,
+    output_years: list
+) -> dict: ...
+
+def run_base_model_from_state(
+    parameters: dict,
+    configuration: str,
+    initial_state: dict,
+    simulation_start_year: int,
+    output_years: list
+) -> dict: ...
+
+def run_base_model_single_year(
+    parameters: dict,
+    configuration: str,
+    initial_state: dict,
+    simulation_start_year: int
+) -> dict: ...
