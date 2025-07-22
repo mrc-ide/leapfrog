dbb729fa3dd11ad99b62f0a576c05d623e78b9f7
Mantra <mantrakusumgar@outlook.com>
2025-07-22 14:31:27 +0100

    add model equality comparison and pytests

--- leapfrog-py/src/leapfrog_py/__init__.py
+++ leapfrog-py/src/leapfrog_py/__init__.py
@@ -1,12 +1,20 @@
-from typing import Union, cast
+from typing import TypeAlias, cast, Union
 from h5py._hl.dataset import Dataset
 import numpy as np
 import h5py
 
-from leapfrog_py._core import run_base_model, run_base_model_from_state, run_base_model_single_year
+from leapfrog_py._core import (
+    run_base_model,
+    run_base_model_from_state,
+    run_base_model_single_year
+)
 
+LeapfrogParameters: TypeAlias = dict[str, Union[np.ndarray, int, float]]
+LeapfrogDataSingleYear: TypeAlias = LeapfrogParameters
+LeapfrogData: TypeAlias = dict[str, np.ndarray]
+LeapfrogRange: TypeAlias = Union[range, list]
 
-def read_h5_file(file_path: str) -> dict[str, Union[np.ndarray, int, float]]:
+def read_h5_file(file_path: str) -> LeapfrogParameters:
     ret = {}
     f = h5py.File(file_path, "r")
     for key in list(f.keys()):
@@ -23,7 +31,7 @@ def read_h5_file(file_path: str) -> dict[str, Union[np.ndarray, int, float]]:
     return ret
 
 
-def save_h5_file(dat: dict[str, Union[np.ndarray, int, float]], file_path: str) -> None:
+def save_h5_file(dat: Union[LeapfrogParameters, LeapfrogData], file_path: str) -> None:
     f = h5py.File(file_path, "w")
     for key in list(dat.keys()):
         arr = dat[key]
@@ -32,3 +40,53 @@ def save_h5_file(dat: dict[str, Union[np.ndarray, int, float]], file_path: str) 
         else:
             f[key] = dat[key]
     f.close()
+
+
+def get_time_slice(state: LeapfrogData, index: int) -> LeapfrogDataSingleYear:
+    return { k: v[..., index] if v.ndim > 1 else v[index].item() for (k, v) in state.items() }
+
+
+def concat_on_time_dim(state1, state2):
+    return { k: np.concatenate((state1[k], state2[k]), axis = -1) for k in state1.keys() }
+
+
+def run_model(
+    parameters: LeapfrogParameters,
+    configuration: str = "HivFullAgeStratification",
+    output_years: LeapfrogRange = range(1970, 2031)
+):
+    return run_base_model(
+        parameters,
+        configuration,
+        output_years
+    )
+
+
+def run_model_from_state(
+    parameters: LeapfrogParameters,
+    configuration: str,
+    initial_state: LeapfrogDataSingleYear,
+    simulation_start_year: int,
+    output_years: LeapfrogRange = range(1970, 2031)
+):
+    return run_base_model_from_state(
+        parameters,
+        configuration,
+        initial_state,
+        simulation_start_year,
+        output_years
+    )
+
+
+def run_model_single_year(
+    parameters: LeapfrogParameters,
+    configuration: str,
+    initial_state: LeapfrogDataSingleYear,
+    simulation_start_year: int
+):
+    return run_base_model_single_year(
+        parameters,
+        configuration,
+        initial_state,
+        simulation_start_year
+    )
