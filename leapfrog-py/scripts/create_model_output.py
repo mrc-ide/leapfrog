a21bd38d1cfeb751dea353a5c8e86fe5ba1ad530
Mantra <mantrakusumgar@outlook.com>
2025-07-22 14:31:27 +0100

    add model equality comparison and pytests

---
+++ leapfrog-py/scripts/create_model_output.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""Run leapfrog model and save output to specified dir
+Usage:
+  run_model <output-dir>
+
+Arguments:
+  <output-dir>  Path to save output to.
+
+Options:
+  -h --help                  Show this screen.
+"""
+
+from docopt import docopt
+import os
+
+from leapfrog_py import read_h5_file, run_model, save_h5_file
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+    output_dir = args["<output-dir>"]
+    
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+
+    parameters = read_h5_file("../r-package/tests/testthat/testdata/adult_parms.h5")
+    ret = run_model(parameters)
+
+    save_h5_file(ret, os.path.join(output_dir, "py-output.h5"))
---
+++ leapfrog-py/tests/test_frogger.py
@@ -0,0 +1,61 @@
+from deepdiff import DeepDiff
+from leapfrog_py import concat_on_time_dim, get_time_slice, run_model, run_model_from_state, run_model_single_year, read_h5_file
+
+
+def assert_equal(obj1, obj2):
+    assert len(DeepDiff(obj1, obj2, ignore_nan_inequality = True)) == 0
+
+
+def test_adult_model_full_strat():
+    parameters = read_h5_file("../r-package/tests/testthat/testdata/adult_parms.h5")
+    ret = run_model(parameters)
+    returned_vars = list(ret.keys())
+    expected_vars = [
+        "p_total_pop", "births", "p_total_pop_background_deaths", "p_hiv_pop",
+        "p_hiv_pop_background_deaths", "h_hiv_adult", "h_art_adult",
+        "h_hiv_deaths_no_art", "p_infections", "h_hiv_deaths_art",
+        "h_art_initiation", "p_hiv_deaths"
+    ]
+    returned_vars.sort()
+    expected_vars.sort()
+    assert_equal(returned_vars, expected_vars)
+
+
+def test_child_model():
+    parameters = read_h5_file("../r-package/tests/testthat/testdata/child_parms.h5")
+    ret = run_model(parameters, "ChildModel")
+    returned_vars = list(ret.keys())
+    expected_vars = [
+        "p_total_pop", "births", "p_total_pop_background_deaths", "p_hiv_pop",
+        "p_hiv_pop_background_deaths", "h_hiv_adult", "h_art_adult",
+        "h_hiv_deaths_no_art", "p_infections", "h_hiv_deaths_art",
+        "h_art_initiation", "p_hiv_deaths", "hc1_hiv_pop", "hc2_hiv_pop",
+        "hc1_art_pop", "hc2_art_pop",
+        "hc1_noart_aids_deaths", "hc2_noart_aids_deaths",
+        "hc1_art_aids_deaths", "hc2_art_aids_deaths", "hiv_births",
+        "hc_art_init", "hc_art_need_init", "ctx_need", "infection_by_type"
+    ]
+    returned_vars.sort()
+    expected_vars.sort()
+    assert_equal(returned_vars, expected_vars)
+
+
+def test_child_model_running_twice_gives_same_result():
+    parameters = read_h5_file("../r-package/tests/testthat/testdata/child_parms.h5")
+    ret1 = run_model(parameters, "ChildModel")
+    ret2 = run_model(parameters, "ChildModel")
+    assert_equal(ret1, ret2)
+
+
+def test_child_model_agrees_on_all_years_two_parts_single_year_runs():
+    parameters = read_h5_file("../r-package/tests/testthat/testdata/child_parms.h5")
+    ret_all_years = run_model(parameters, "ChildModel")
+
+    ret_first_half = run_model(parameters, "ChildModel", range(1970, 2001))
+    ret_second_half = run_model_from_state(parameters, "ChildModel", get_time_slice(ret_first_half, 30), 2000, range(2001, 2031))
+    assert_equal(ret_all_years, concat_on_time_dim(ret_first_half, ret_second_half))
+
+    ret_single_year = get_time_slice(run_model(parameters, "ChildModel", [1970]), 0)
+    for year in range(1971, 2031):
+        ret_single_year = run_model_single_year(parameters, "ChildModel", ret_single_year, year - 1)
+        assert_equal(ret_single_year, get_time_slice(ret_all_years, year - 1970))
