d641970caa042c3b1d87c6d1df1e1f1a6c96f295
mwalte10 <maggiek925@gmail.com>
2025-08-11 15:30:53 +0100

    changed index order of hc_art_grad, which was incorrect and causing git tests to fail. have fixed and tested on my machine but pushing to see if this fixes issue

--- r-package/inst/include/generated/config.hpp
+++ r-package/inst/include/generated/config.hpp
@@ -783,10 +783,10 @@ struct HcConfig {
     >;
     nda::array<real_type, shape_total_art_this_year> total_art_this_year;
     using shape_hc_art_grad = nda::shape<
-      nda::dim<0, SS::hDS, 1>,
-      nda::dim<0, SS::hcTT, (SS::hDS)>,
-      nda::dim<0, SS::hAG, (SS::hDS) * (SS::hcTT)>,
-      nda::dim<0, SS::NS, (SS::hDS) * (SS::hcTT) * (SS::hAG)>
+      nda::dim<0, SS::hcTT, 1>,
+      nda::dim<0, SS::hDS, (SS::hcTT)>,
+      nda::dim<0, SS::hAG, (SS::hcTT) * (SS::hDS)>,
+      nda::dim<0, SS::NS, (SS::hcTT) * (SS::hDS) * (SS::hAG)>
     >;
     nda::array<real_type, shape_hc_art_grad> hc_art_grad;
     using shape_hc_art_scalar = nda::shape<
