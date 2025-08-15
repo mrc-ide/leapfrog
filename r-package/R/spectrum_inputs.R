498410355c5e2d898df3e9adb1902d05153cade0
Mantra <mantrakusumgar@outlook.com>
2025-08-15 12:08:28 +0100

    jeff comment

--- r-package/R/spectrum_inputs.R
+++ r-package/R/spectrum_inputs.R
@@ -216,7 +216,10 @@ prepare_leapfrog_projp <- function(pjnz, hiv_steps_per_year = 10L, hTS = 3) {
   v$incidpopage <- attr(v$incidinput, "incidpopage")
   v$incrr_sex <- projp$incrr_sex
 
-  v$pAG_INCIDPOP <- ifelse(v$incidpopage == 0L, 35L, 66L)
+  pAG_15_to_49 <- 35L
+  pAG_15_plus <- 66L
+
+  v$pAG_INCIDPOP <- ifelse(v$incidpopage == 0L, pAG_15_to_49, pAG_15_plus)
   v$pIDX_INCIDPOP <- 15L
 
   v$artmx_timerr <- projp$artmx_timerr[c(1, 2, rep(3, hTS - 2)), ]
